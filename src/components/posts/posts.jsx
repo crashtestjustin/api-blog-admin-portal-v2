79103a591c3fb8c578bd524e1120db21264ebe69
Justin Elliott <thecrashtestjustin@gmail.com>
2024-02-05 07:25:08 -0800

    create the main posts component layout

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { OutletContext } from "../../App";
 import { Home } from "../home/home";
 import { checkAccessTokenStatus } from "../utility";
+import { Link } from "react-router-dom";
+import { getAllPosts } from "../api";
+import styles from "./posts.module.css";
 
 function Posts() {
   const { isLoggedIn, setIsLoggedIn } = useContext(OutletContext);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const checkTokenStatus = async () => {
@@ -15,10 +19,48 @@ function Posts() {
     checkTokenStatus();
   }, []);
 
+  useEffect(() => {
+    const getAllThePosts = async () => {
+      const allPosts = await getAllPosts();
+      setPosts(allPosts.posts);
+    };
+    getAllThePosts();
+  }, []);
+
+  console.log(posts);
+
   return (
     <>
       {isLoggedIn ? (
-        <p>Posts go here</p>
+        <>
+          <div className={styles.header}>
+            <h1>Posts</h1>
+          </div>
+          <div className={styles.postsSection}>
+            {posts ? (
+              posts.map((post) => (
+                <Link to={`/posts/${post._id}`} key={post._id}>
+                  <div className={styles.post}>
+                    <div className={styles.title}>
+                      <h3>{post.title}</h3>
+                    </div>
+                    <div className={styles.postStatus}>
+                      <p>
+                        Status:{" "}
+                        {post.published === false ? "Unpublished" : "Published"}
+                      </p>
+                    </div>
+                    <div className={styles.body}>
+                      <p>{post.body}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <p>No posts found</p>
+            )}
+          </div>
+        </>
       ) : (
         <>
           <p>Posts unnaccessible while logged out.</p>
